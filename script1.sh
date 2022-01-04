fffc8b23cb2966acc2b45c2bd7761f86000de920
NirGrossman1986 <82283999+NirGrossman1986@users.noreply.github.com>
2022-01-04 14:18:08 +0200

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -7,5 +7,7 @@ sudo apt -y upgrade < "/dev/null"
 # Download the Terraform from S3
 sudo aws s3 cp s3://workshop-tf-state-nirg/terraform_destroy.sh ~/
 sudo aws s3 cp s3://workshop-tf-state-nirg/terraform_apply.sh ~/
+sudo apt -y install zip unzip
 sudo aws s3 cp s3://workshop-tf-state-nirg/terraform_project.zip ~/
+sudo unzip ~/terraform_project.zip
 
