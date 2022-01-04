760b48f1a7e7e2a53f628f732a30370f65597dea
NirGrossman1986 <82283999+NirGrossman1986@users.noreply.github.com>
2022-01-04 14:39:05 +0200

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -9,5 +9,6 @@ sudo aws s3 cp s3://workshop-tf-state-nirg/terraform_destroy.sh ~/
 sudo aws s3 cp s3://workshop-tf-state-nirg/terraform_apply.sh ~/
 sudo apt -y install zip unzip
 sudo aws s3 cp s3://workshop-tf-state-nirg/terraform_project.zip ~/
+cd ~
 sudo unzip ~/terraform_project.zip
 
